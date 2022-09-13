09c6526818e953cf933e3028697f9ddc340a6807
Rinse <rinse12@tutanota.com>
2022-09-13 08:41:40 +0000

    Added skeleton for "plebbit get" command using commander.js

---
+++ src/actions.ts
@@ -0,0 +1,3 @@
+export function get(input: string, options) {
+    console.log(`plebbit get:  (input: ${input}, options: ${JSON.stringify(options)})`);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello world!");
+import { program } from "commander";
+import constants from "./constants";
+import * as actions from "./actions";
+import { version } from "../package.json";
+
+program.name("plebbit-cli").description(constants.PLEBBIT_CLI_DESCRIPTION).version(version);
+
+program
+    .command("get")
+    .description(constants.CMD_GET)
+    .argument("<cid/domain>", "CID of comment/page/subplebbit or domain for subplebbit")
+    .action(actions.get);
+
+program.parse(process.argv);
