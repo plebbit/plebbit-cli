5d55f505c1c95bb90ed1f03edcfa15cd7f17d8f0
Rinse <rinse12@tutanota.com>
2022-10-05 09:08:09 +0000

    Implemented swagger-ui docs page on /api/v0/docs

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,6 +1,10 @@
 // src/app.ts
-import express, { json } from "express";
+import express, { Response as ExResponse, Request as ExRequest, json } from "express";
 import { RegisterRoutes } from "../../build/routes.js";
+import swaggerUi from "swagger-ui-express";
+import fs from "fs-extra";
+
+const swaggerHtml = swaggerUi.generateHTML(JSON.parse((await fs.promises.readFile("build/swagger.json")).toString()));
 
 export function startApi(apiPort: number) {
     const app = express();
@@ -8,6 +12,10 @@ export function startApi(apiPort: number) {
     app.use(json());
 
     RegisterRoutes(app);
+
+    app.use("/api/v0/docs", swaggerUi.serve, async (_req: ExRequest, res: ExResponse) => {
+        return res.send(swaggerHtml);
+    });
 
     app.listen(apiPort, () => console.log(`Example app listening at http://localhost:${apiPort}`));
 }
