e73516a9db6f778e44ddc3e0346b620714ed230f
Rinse <rinse12@tutanota.com>
2022-10-07 14:42:59 +0000

    implemented /api/v0/subplebbit/create

--- src/api/subplebbitController.ts
+++ src/api/subplebbitController.ts
@@ -1,16 +1,27 @@
 import { RUNNING_SUBPLEBBITS } from "@plebbit/plebbit-js/dist/node/subplebbit.js";
-import { Controller, Post, Route } from "tsoa";
+import { CreateSubplebbitOptions, SubplebbitType } from "@plebbit/plebbit-js/dist/node/types.js";
+import { Controller, Post, Route, Body } from "tsoa";
 import { SubplebbitList } from "../types.js";
 import { sharedSingleton } from "./server.js";
 
 @Route("/api/v0/subplebbit")
 export class SubplebbitController extends Controller {
-    @Post("/list")
+    @Post("list")
     public async list(): Promise<SubplebbitList> {
         return Promise.all(
             (await sharedSingleton.plebbit.listSubplebbits()).map((subAddress) => {
                 return { address: subAddress, started: Boolean(RUNNING_SUBPLEBBITS[subAddress]) };
             })
         );
+    }
+
+    /**
+     * @example requestBody { "title": "Memes", "description": "Post your memes here." }
+     * @param requestBody The initial fields of the subplebbit. Could also be {address} to retrieve a subplebbit that has been created in the past
+     * @returns Subplebbit after being created. Will omit signer and senstive fields
+     */
+    @Post("create")
+    public async create(@Body() requestBody: CreateSubplebbitOptions): Promise<SubplebbitType> {
+        return (await sharedSingleton.plebbit.createSubplebbit(requestBody)).toJSON();
     }
 }
