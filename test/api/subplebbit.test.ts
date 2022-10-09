1cd20649561e87f37de6f06479b2c152bcb6aba4
Rinse <rinse12@tutanota.com>
2022-10-09 08:19:59 +0000

    Added checks for statusCode and statusText. Added tests for /api/v0/subplebbit/start and list

--- test/api/subplebbit.test.ts
+++ test/api/subplebbit.test.ts
@@ -1,16 +1,27 @@
 import fetch from "node-fetch";
 import { expect } from "chai";
+import { statusCodes, statusMessages } from "../../src/api/ResponseStatuses.js";
 import { CreateSubplebbitOptions, SubplebbitType } from "@plebbit/plebbit-js/dist/node/types.js";
 import { SubplebbitList } from "../../src/types.js";
+import Plebbit from "@plebbit/plebbit-js";
+
+if (
+    typeof process.env["PLEBBIT_API_PORT"] !== "string" ||
+    typeof process.env["IPFS_PUBSUB_PORT"] !== "string" ||
+    typeof process.env["IPFS_PORT"] !== "string"
+)
+    throw Error("You need to set all env variables PLEBBIT_API_PORT, IPFS_PUBSUB_PORT, IPFS_PORT");
+
 const baseUrl = `http://localhost:${process.env["PLEBBIT_API_PORT"]}/api/v0/subplebbit`;
-let createdSubplebbit: SubplebbitType;
+
 describe("/api/v0/subplebbit/create", async () => {
     const createUrl = `${baseUrl}/create`;
+    let createdSubplebbit: SubplebbitType;
     it("Request fails with documented error if invalid json was provided", async () => {
         const invalidJson: string = JSON.stringify({ test: "zz" }).slice(1); // Delete first character, should make the string unparsable
         const res = await fetch(createUrl, { method: "POST", body: invalidJson, headers: { "content-type": "application/json" } });
-        expect(res.status).to.equal(400);
-        expect(res.statusText).to.equal("Request body is invalid as a JSON");
+        expect(res.status).to.equal(statusCodes.ERR_INVALID_JSON_FOR_REQUEST_BODY);
+        expect(res.statusText).to.equal(statusMessages.ERR_INVALID_JSON_FOR_REQUEST_BODY);
     });
 
     it(`Can create a new subplebbit successfully`, async () => {
@@ -23,7 +34,8 @@ describe("/api/v0/subplebbit/create", async () => {
             body: JSON.stringify(subProps),
             headers: { "content-type": "application/json" }
         });
-        expect(res.status).to.equal(200);
+        expect(res.status).to.equal(statusCodes.SUCCESS_SUBPLEBBIT_CREATED);
+        expect(res.statusText).to.equal(statusMessages.SUCCESS_SUBPLEBBIT_CREATED);
         createdSubplebbit = <SubplebbitType>await res.json();
         expect(createdSubplebbit.address).to.be.a.string;
         expect(createdSubplebbit.createdAt).to.be.a("number");
@@ -42,17 +54,71 @@ describe("/api/v0/subplebbit/create", async () => {
             body: JSON.stringify(subProps),
             headers: { "content-type": "application/json" }
         });
-        expect(res.status).to.equal(200);
+        expect(res.status).to.equal(statusCodes.SUCCESS_SUBPLEBBIT_CREATED);
         const retrievedSubplebbit: SubplebbitType = <SubplebbitType>await res.json();
 
         expect(retrievedSubplebbit).to.deep.equal(createdSubplebbit);
     });
 });
 
+describe(`/api/v0/subplebbit/start`, async () => {
+    let startedSubplebbit: SubplebbitType;
+    before(async () => {
+        startedSubplebbit = <SubplebbitType>await (
+            await fetch(`${baseUrl}/create`, {
+                method: "POST",
+                body: JSON.stringify({ title: "test" + Date.now() }),
+                headers: { "content-type": "application/json" }
+            })
+        ).json();
+    });
+    it(`A started subplebbit can receive challenges`, async () => {
+        return new Promise(async (resolve) => {
+            const startRes = await fetch(`${baseUrl}/start?address=${startedSubplebbit.address}`, { method: "POST" });
+            expect(startRes.status).to.equal(statusCodes.SUCCESS_SUBPLEBBIT_STARTED);
+            expect(startRes.statusText).to.equal(statusMessages.SUCCESS_SUBPLEBBIT_STARTED);
+
+            const plebbit = await Plebbit({
+                ipfsHttpClientOptions: `http://localhost:${process.env["IPFS_PORT"]}/api/v0`,
+                pubsubHttpClientOptions: `http://localhost:${process.env["IPFS_PUBSUB_PORT"]}/api/v0`
+            });
+
+            const mockPost = await plebbit.createComment({
+                subplebbitAddress: startedSubplebbit.address,
+                title: "test" + Date.now(),
+                signer: await plebbit.createSigner()
+            });
+
+            await mockPost.publish({});
+
+            mockPost.once("challenge", resolve); // This test is done once we receive a challenge
+        });
+    });
+
+    it(`Request fails with documented error if subplebbit is already started`, async () => {
+        const startRes = await fetch(`${baseUrl}/start?address=${startedSubplebbit.address}`, { method: "POST" });
+        expect(startRes.status).to.equal(statusCodes.ERR_SUB_ALREADY_STARTED);
+        expect(startRes.statusText).to.equal(statusMessages.ERR_SUB_ALREADY_STARTED);
+    });
+    it(`Request fails with documented error if subplebbit has not been created`, async () => {
+        const startRes = await fetch(`${baseUrl}/start?address=gibbreish`, { method: "POST" });
+        expect(startRes.status).to.equal(statusCodes.ERR_SUBPLEBBIT_DOES_NOT_EXIST);
+        expect(startRes.statusText).to.equal(statusMessages.ERR_SUBPLEBBIT_DOES_NOT_EXIST);
+    });
+});
+
 describe(`/api/v0/subplebbit/list`, async () => {
     const listUrl = `${baseUrl}/list`;
+    let createdSubplebbit: SubplebbitType;
 
     it(`Newly created subplebbit is listed`, async () => {
+        createdSubplebbit = <SubplebbitType>await (
+            await fetch(`${baseUrl}/create`, {
+                method: "POST",
+                body: JSON.stringify({}),
+                headers: { "content-type": "application/json" }
+            })
+        ).json();
         const res = await fetch(listUrl, {
             method: "POST"
         });
@@ -60,6 +126,20 @@ describe(`/api/v0/subplebbit/list`, async () => {
         const createdSubFromList = subs.filter((sub) => sub.address === createdSubplebbit.address)[0];
         expect(createdSubFromList).to.be.a("object");
         expect(createdSubFromList?.address).to.equal(createdSubplebbit.address);
+        expect(createdSubFromList?.started).to.be.false;
+    });
+
+    it(`Started subplebbit has started === true`, async () => {
+        await fetch(`${baseUrl}/start?address=${createdSubplebbit.address}`, { method: "POST" });
+        const subs: SubplebbitList = <SubplebbitList>await (await fetch(listUrl, { method: "POST" })).json();
+        const createdSubFromList = subs.filter((sub) => sub.address === createdSubplebbit.address)[0];
+        expect(createdSubFromList).to.be.a("object");
+        expect(createdSubFromList?.started).to.be.true;
+    });
+    it(`Stopping subplebbit with /api/v0/subplebbit/stop changes .started to false`, async () => {
+        await fetch(`${baseUrl}/stop?address=${createdSubplebbit.address}`, { method: "POST" });
+        const subs: SubplebbitList = <SubplebbitList>await (await fetch(listUrl, { method: "POST" })).json();
+        const createdSubFromList = subs.filter((sub) => sub.address === createdSubplebbit.address)[0];
         expect(createdSubFromList?.started).to.be.false;
     });
 });
