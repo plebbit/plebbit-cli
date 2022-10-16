4ab5467dc67f745c20aeb8b94c428b6a12689a29
Rinse <rinse12@tutanota.com>
2022-10-16 12:04:02 +0000

    kill daemon after test

--- test/cli/main.test.ts
+++ test/cli/main.test.ts
@@ -7,20 +7,24 @@ import fetch from "node-fetch";
 import { SubplebbitList } from "../../src/types.js";
 
 describe("plebbit daemon", async () => {
-    const startDaemon = (args: string[]) => {
+    const startDaemon = (args: string[]): Promise<{ pid: number }> => {
         return new Promise(async (resolve, reject) => {
             const spawedProcess = spawn("node", [".", "daemon", ...args]);
             spawedProcess.on("exit", (exitCode, signal) => {
                 reject(`spawnAsync process '${spawedProcess.pid}' exited with code '${exitCode}' signal '${signal}'`);
             });
             spawedProcess.stdout.on("data", (data) => {
-                if (data.toString().match("You can find API documentation")) resolve(null);
+                if (data.toString().match("You can find API documentation")) {
+                    if (!spawedProcess.pid) throw Error(`process ID is not defined`);
+                    spawedProcess.removeAllListeners();
+                    resolve({ pid: spawedProcess.pid });
+                }
             });
             spawedProcess.on("error", (data) => reject(data));
         });
     };
     it(`Starts a daemon successfully with default args`, async () => {
-        await startDaemon([]);
+        const { pid: daemonpid } = await startDaemon([]);
         const plebbit = await Plebbit({ ipfsHttpClientOptions: `http://localhost:${defaults.IPFS_API_PORT}` });
         expect(plebbit.ipfsGatewayUrl).to.equal(`http://127.0.0.1:${defaults.IPFS_GATEWAY_PORT}`);
         // We're able to retrieve ipfs gateway url from ipfs node, that means ipfs node was ran correctly
@@ -29,5 +33,6 @@ describe("plebbit daemon", async () => {
         });
         const subs: SubplebbitList = <SubplebbitList>await res.json();
         expect(Array.isArray(subs)).to.be.true;
+        process.kill(daemonpid);
     });
 });
