bee6eeec22e6ac46fbf3733f53e604f943ddebb3
Rinse <rinse12@tutanota.com>
2022-10-23 13:01:51 +0000

    Added a test for the aftermath of killing plebbit daemon

--- test/cli/main.test.ts
+++ test/cli/main.test.ts
@@ -1,15 +1,24 @@
 // This file is to test root commands like `plebbit daemon` or `plebbit get`, whereas commands like `plebbit subplebbit start` are considered nested
 import Plebbit from "@plebbit/plebbit-js";
-import { expect } from "chai";
-import { spawn } from "child_process";
+import { ChildProcessWithoutNullStreams, spawn } from "child_process";
 import defaults from "../../src/cli/defaults.js";
 import fetch from "node-fetch";
 import { SubplebbitList } from "../../src/types.js";
+import { Plebbit as PlebbitClass } from "@plebbit/plebbit-js/dist/node/plebbit.js";
+import chai from "chai";
+//@ts-ignore
+import chaiAsPromised from "chai-as-promised";
+chai.use(chaiAsPromised);
+
+const { expect, assert } = chai;
 
 describe("plebbit daemon", async () => {
-    const startDaemon = (args: string[]): Promise<{ pid: number }> => {
+    let plebbit: PlebbitClass;
+    let daemonProcess: ChildProcessWithoutNullStreams;
+
+    const startDaemon = (args: string[]): Promise<{ process: ChildProcessWithoutNullStreams }> => {
         return new Promise(async (resolve, reject) => {
-            const spawedProcess = spawn("node", [".", "daemon", ...args]);
+            const spawedProcess = spawn("node", [".", "daemon", ...args], { env: process.env });
             spawedProcess.on("exit", (exitCode, signal) => {
                 reject(`spawnAsync process '${spawedProcess.pid}' exited with code '${exitCode}' signal '${signal}'`);
             });
@@ -17,15 +26,20 @@ describe("plebbit daemon", async () => {
                 if (data.toString().match("You can find API documentation")) {
                     if (!spawedProcess.pid) throw Error(`process ID is not defined`);
                     spawedProcess.removeAllListeners();
-                    resolve({ pid: spawedProcess.pid });
+                    resolve({ process: spawedProcess });
                 }
             });
             spawedProcess.on("error", (data) => reject(data));
         });
     };
+
+    before(async () => {
+        daemonProcess = (await startDaemon([])).process;
+    });
     it(`Starts a daemon successfully with default args`, async () => {
-        const { pid: daemonpid } = await startDaemon([]);
-        const plebbit = await Plebbit({ ipfsHttpClientOptions: `http://localhost:${defaults.IPFS_API_PORT}` });
+        expect(daemonProcess.pid).to.be.a("number");
+        expect(daemonProcess.killed).to.be.false;
+        plebbit = await Plebbit({ ipfsHttpClientOptions: `http://localhost:${defaults.IPFS_API_PORT}` });
         expect(plebbit.ipfsGatewayUrl).to.equal(`http://127.0.0.1:${defaults.IPFS_GATEWAY_PORT}`);
         // We're able to retrieve ipfs gateway url from ipfs node, that means ipfs node was ran correctly
         const res = await fetch(`http://localhost:${defaults.PLEBBIT_API_PORT}/api/v0/subplebbit/list`, {
@@ -33,6 +47,23 @@ describe("plebbit daemon", async () => {
         });
         const subs: SubplebbitList = <SubplebbitList>await res.json();
         expect(Array.isArray(subs)).to.be.true;
-        process.kill(daemonpid);
+    });
+
+    it(`Ipfs node is killed after killing plebbit daemon`, async () => {
+        expect(daemonProcess.kill()).to.be.true;
+
+        // Test whether daemon is reachable, it should not be reachable
+        //@ts-ignore
+        await assert.isRejected(
+            fetch(`http://localhost:${defaults.PLEBBIT_API_PORT}/api/v0/subplebbit/list`, {
+                method: "POST"
+            }),
+            "ECONNRESET",
+            ""
+        );
+
+        // Plebbit should fail to retrieve gateway from ipfs node since it's killed. Will default to cloudflare after
+        plebbit = await Plebbit({ ipfsHttpClientOptions: `http://localhost:${defaults.IPFS_API_PORT}` });
+        expect(plebbit.ipfsGatewayUrl).to.equal(`https://cloudflare-ipfs.com`);
     });
 });
