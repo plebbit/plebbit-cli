98023d9ee0bd438da49b8e890b9bb734fe73190e
Rinse <rinse12@tutanota.com>
2022-11-22 17:02:38 +0000

    Added cli test for "plebbit subplebbit create". Will expand

---
+++ test/cli/create.subplebbit.test.ts
@@ -0,0 +1,43 @@
+import { expect, test } from "@oclif/test";
+import { statusCodes } from "../../src/api/responseStatuses.js";
+import { CreateSubplebbitOptions } from "../../src/cli/types.js";
+import defaults from "../../src/common-utils/defaults.js";
+//@ts-ignore
+import DataObjectParser from "dataobject-parser";
+
+describe("plebbit subplebbit create", () => {
+    const createOptions: CreateSubplebbitOptions = {
+        signer: { privateKey: "testPrivateKey" },
+        database: { connection: { filename: "testFilename" } },
+        title: "testTitle",
+        description: "testDescription",
+        pubsubTopic: "testPubsubTopic",
+        suggested: {
+            primaryColor: "testPrimaryColor",
+            secondaryColor: "testSecondaryColor",
+            avatarUrl: "http://localhost:8080/avatar.png",
+            bannerUrl: "http://localhost:8080/banner.png",
+            backgroundUrl: "http://localhost:8080/background.png",
+            language: "testLanguage"
+        }
+    };
+    const createOptionsFlattened = DataObjectParser.untranspose(createOptions);
+    test.nock(`http://localhost:${defaults.PLEBBIT_API_PORT}/api/v0`, (api) =>
+        api
+            .post("/subplebbit/create")
+            .reply((_, requestBody) => [statusCodes.SUCCESS_SUBPLEBBIT_CREATED, requestBody])
+            .post("/subplebbit/list")
+            .reply(200, [])
+    )
+        .loadConfig({ root: process.cwd() })
+        .stdout()
+        .command(["subplebbit create"].concat(Object.entries(createOptionsFlattened).map(([key, value]) => `--${key}=${value}`)))
+        .it(`Parse create options correctly`, (ctx) => {
+            expect(JSON.parse(ctx.stdout)).to.deep.equal(createOptions);
+            expect(ctx.error).to.be.undefined;
+        });
+
+    // Test: will throw error if daemon is down
+    // Test: show error
+    // Test: Show correct output
+});
