1882d95d2cac255d1e3677abd092ebd744e80894
Rinse <rinse12@tutanota.com>
2022-12-07 06:43:57 +0000

    test(cli-subplebbit-create): correct --signer.privateKey to privateKeyPath in create test

--- test/cli/create.subplebbit.test.ts
+++ test/cli/create.subplebbit.test.ts
@@ -2,12 +2,14 @@ import { expect, test } from "@oclif/test";
 import { statusCodes } from "../../dist/src/api/response-statuses.js";
 import { CreateSubplebbitOptions } from "../../src/cli/types.js";
 import defaults from "../../dist/src/common-utils/defaults.js";
+import signers from "../fixtures/signers";
 //@ts-ignore
 import DataObjectParser from "dataobject-parser";
+import { SubplebbitType } from "@plebbit/plebbit-js/dist/node/types.js";
 
 describe("plebbit subplebbit create", () => {
     const createOptions: CreateSubplebbitOptions = {
-        signer: { privateKey: "testPrivateKey" },
+        privateKeyPath: "test/fixtures/sub_0_private_key.pem",
         title: "testTitle",
         description: "testDescription",
         pubsubTopic: "testPubsubTopic",
@@ -24,7 +26,10 @@ describe("plebbit subplebbit create", () => {
     test.nock(`http://localhost:${defaults.PLEBBIT_API_PORT}/api/v0`, (api) =>
         api
             .post("/subplebbit/create")
-            .reply((_, requestBody) => [statusCodes.SUCCESS_SUBPLEBBIT_CREATED, requestBody])
+            .reply((_, requestBody) => [statusCodes.SUCCESS_SUBPLEBBIT_CREATED, { address: signers[0]?.address, ...createOptions }])
+            .post(`/subplebbit/start?address=${signers[0]!.address}`)
+            .reply((_, requestBody) => [statusCodes.SUCCESS_SUBPLEBBIT_STARTED, requestBody])
+
             .post("/subplebbit/list")
             .reply(200, [])
     )
@@ -32,7 +37,7 @@ describe("plebbit subplebbit create", () => {
         .stdout()
         .command(["subplebbit create"].concat(Object.entries(createOptionsFlattened).map(([key, value]) => `--${key}=${value}`)))
         .it(`Parse create options correctly`, (ctx) => {
-            expect(JSON.parse(ctx.stdout)).to.deep.equal(createOptions);
-            expect(ctx.error).to.be.undefined;
+            expect(JSON.parse(ctx.stdout)).to.deep.equal({ ...createOptions, address: signers[0]!.address });
+        });
         });
 });
---
+++ test/fixtures/signers.ts
@@ -0,0 +1,28 @@
+export default [
+    {
+        type: "rsa",
+        publicKey: `-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAuJSnnxopT40LZqoBvnq6
+n2d8GlmlIs9RH5Qtqyroc7KggWvUyT/ETgqFsGBj6kIXr0JtAXgHYbvnUuGcNGB6
+Ms1lVdcP4S60UV7lPOGuGan5XMCk3keQ24psyMvJVGRdO+NZRcohNelEU4MmLrpB
+nfqwM8eN0Yzb6xadCVS+dVL9NzV7cKkvLdwaCJsCYo8s9N0Yz3OuOwiwB/B8g21G
+A8QOrEBYnGQ+gABDVuINEte9pm5ciV3oiHQpzsCWljIXgsZLfUgLq0kD3De2Pqrp
+sdl+hK3bw/O8aMucwVx6tcctktylOY3YYjM+QepcZT+7QuzE0KbPyNIRL9SDT4HR
+xQIDAQAB
+-----END PUBLIC KEY-----`,
+        address: "QmbdJpNU6cAgSXHjUNnSBrUZGBtStKPkdwKyiffqRy1x6c"
+    },
+    {
+        type: "rsa",
+        publicKey: `-----BEGIN PUBLIC KEY-----
+    MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAx8cr7kdY5iPRFkVROYvR
+    70espYe/ESffUhz3hy2eZ6ZrW3yC58Cb+V3WKRHk7jC7XxInvwUM2LXYJAij5jbA
+    SF/EdEc07NlghlChncGfQ6pj7Ngr+c0sUNFmfHAvuuMGr9amb+MG+cmEFituhhRX
+    wtsGEEJ+dqeXA4DW3EigAynbFYZqsCl603PieIE6iymiay3J9HFi/iBtd2DMp93g
+    8J/j4htmUHUZysJbT/7RO4gs6dkCmu2ijXM5yOiSwQeHFhqpZ7e3BEMmGXQJJ52c
+    nPF333arQKlX9sveeyqEISdZh2ts6Wp/AjGreAKcHEY9cMrSe69ZCsc5Ki5wHGGi
+    twIDAQAB
+    -----END PUBLIC KEY-----`,
+        address: "QmTKb1CQF6n6VUuiF5UCYMft2SRWRUuhkYq21vDiNmwWSG"
+    }
+];
