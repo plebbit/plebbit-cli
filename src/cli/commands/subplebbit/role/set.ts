d16d0abfdf8e4c8a453d6f25e36d053c0ada267d
Rinse <rinse12@tutanota.com>
2022-12-14 10:38:42 +0000

    feat(cli): a cli command to set roles for authors within a subplebbit
    `plebbit subplebbit role set <subplebbit-address> <author-address> --role=<role>` will be used to
    set roles like moderator for authors within the subplebbit

---
+++ src/cli/commands/subplebbit/role/set.ts
@@ -0,0 +1,71 @@
+import { Flags } from "@oclif/core";
+import Logger from "@plebbit/plebbit-logger";
+import { BaseCommand } from "../../../base-command.js";
+import fetch from "node-fetch";
+import { SubplebbitRole, SubplebbitType } from "@plebbit/plebbit-js/dist/node/types.js";
+import { statusCodes, statusMessages } from "../../../../api/response-statuses.js";
+import { exitStatuses } from "../../../exit-codes.js";
+
+export default class Set extends BaseCommand {
+    static override description =
+        "Set role to an author within the subplebbit. If an author has a role already, it would get overidden with the new role";
+
+    static override examples = ["plebbit role set plebbit.eth estebanabaroa.eth --role admin"];
+
+    static override flags = {
+        role: Flags.enum<SubplebbitRole["role"]>({
+            options: ["admin", "moderator", "owner"],
+            description: "New role for the author",
+            default: "moderator",
+            required: true
+        })
+    };
+
+    static override args = [
+        {
+            name: "subplebbitAddress",
+            required: true, // make the arg required with `required: true`
+            description: "Address of subplebbit"
+        },
+        {
+            name: "authorAddress",
+            required: true,
+            description: "The address of the author to set the role to"
+        }
+    ];
+
+    async run(): Promise<void> {
+        const { flags, args } = await this.parse(Set);
+
+        const log = Logger("plebbit-cli:commands:subplebbit:roles:set");
+        log(`flags: `, flags);
+        log(`args: `, args);
+        const authorAddress: string = args["authorAddress"];
+        const subplebbitAddress: string = args["subplebbitAddress"];
+        await this.stopIfDaemonIsDown(flags.apiUrl.toString());
+
+        const subRes = await fetch(`${flags.apiUrl}/subplebbit/create`, {
+            method: "POST",
+            body: JSON.stringify({ address: subplebbitAddress })
+        });
+        if (subRes.status === statusCodes.ERR_SUBPLEBBIT_DOES_NOT_EXIST)
+            this.error(statusMessages.ERR_SUBPLEBBIT_DOES_NOT_EXIST, {
+                code: "ERR_SUBPLEBBIT_DOES_NOT_EXIST",
+                exit: exitStatuses.ERR_SUBPLEBBIT_DOES_NOT_EXIST
+            });
+        if (subRes.status !== statusCodes.SUCCESS_SUBPLEBBIT_CREATED) this.error(subRes.statusText);
+        const sub: SubplebbitType = await subRes.json();
+
+        const newRoles: SubplebbitType["roles"] = { ...sub.roles, [authorAddress]: { role: flags.role } };
+
+        const editRes = await fetch(`${flags.apiUrl}/subplebbit/edit?address=${sub.address}`, {
+            body: JSON.stringify({ roles: newRoles }),
+            method: "POST",
+            headers: { "content-type": "application/json" }
+        });
+
+        if (editRes.status !== statusCodes.SUCCESS_SUBPLEBBIT_EDITED)
+            // TODO, status text is not enough to explain error. Include more info
+            this.error(editRes.statusText);
+    }
+}
