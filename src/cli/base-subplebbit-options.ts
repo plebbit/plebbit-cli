0521c17bcd58e051e0e8b44a1057cd5cfb50df53
Rinse <rinse12@tutanota.com>
2023-03-30 17:29:40 +0000

    feat(cli): add settings field to subplebbit edit in CLI

--- src/cli/base-subplebbit-options.ts
+++ src/cli/base-subplebbit-options.ts
@@ -8,11 +8,15 @@ export default abstract class BaseSubplebbitOptions extends BaseCommand {
         description: Flags.string({ summary: "Description of the subplebbit" }),
         pubsubTopic: Flags.string({ summary: "The string to publish to in the pubsub, a public key of the subplebbit owner's choice" }),
 
-        "suggested.primaryColor": Flags.string({ sumamry: "Primary color of the subplebbit in hex" }),
+        "suggested.primaryColor": Flags.string({ summary: "Primary color of the subplebbit in hex" }),
         "suggested.secondaryColor": Flags.string({ summary: "Secondary color of the subplebbit in hex" }),
         "suggested.avatarUrl": Flags.url({ summary: "The URL of the subplebbit's avatar" }),
         "suggested.bannerUrl": Flags.url({ summary: "The URL of the subplebbit's banner" }),
         "suggested.backgroundUrl": Flags.url({ summary: "The URL of the subplebbit's background" }),
-        "suggested.language": Flags.string({ summary: "The language of the subplebbit" })
+        "suggested.language": Flags.string({ summary: "The language of the subplebbit" }),
+
+        // Settings
+        "settings.fetchThumbnailUrls": Flags.boolean({"summary": "Fetch the thumbnail URLs of comments with comment.link property, could reveal the IP address of the subplebbit node"}),
+        "settings.fetchThumbnailUrlsProxyUrl": Flags.string({"summary": "The HTTP proxy URL used to fetch thumbnail URLs"})
     };
 }
