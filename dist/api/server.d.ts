a1e8c3c01de14814ea21f876c1583efbcd07ed2c
Rinse12 <Rinse12@users.noreply.github.com>
2023-11-13 17:17:24 +0000

    build(packaging): ci build

--- dist/api/server.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function startRpcServer(plebbitRpcApiPort: number, ipfsApiEndpoint: string, ipfsGatewayEndpoint: string, plebbitDataPath: string, seedSubs: string[] | undefined): Promise<void>;
