c5b09dadc6092d68f6c76c83d0cbaf9635157b54
Rinse <rinse12@tutanota.com>
2024-02-17 17:37:16 +0000

    build(executable): rewrite the logic of generating an executable. No more single file executable

---
+++ config/rename_tarballs_installers.ts
@@ -0,0 +1,30 @@
+import fs from "fs/promises";
+import path from "path";
+
+(async () => {
+    // Rename tarball and windows installers to be ready for CI upload
+    const distDirectory = path.join(process.cwd(), "dist");
+
+    const tarballFilenames = await fs.readdir(distDirectory);
+
+    for (const tarballFilename of tarballFilenames) {
+        if (!tarballFilename.endsWith(".tar.gz")) continue;
+        const splits = tarballFilename.split("-");
+        const oldTarballPath = path.join(distDirectory, tarballFilename);
+        const newTarbellPath = `plebbit_${splits[3]}-${splits[4]}`;
+        await fs.rename(oldTarballPath, newTarbellPath);
+        console.log(`Renamed ${oldTarballPath} to ${newTarbellPath}`);
+    }
+
+    // Rename windows installer files here
+    const windowsInstallerDir = path.join(distDirectory, "win32");
+    const winInstallerFilenames = await fs.readdir(windowsInstallerDir);
+    for (const winInstallerFilename of winInstallerFilenames) {
+        if (!winInstallerFilename.endsWith(".exe")) continue;
+        const splits = winInstallerFilename.split("-");
+        const oldWinInstallerPath = path.join(windowsInstallerDir, winInstallerFilename);
+        const newWinInstallerPath = `plebbit_installer_win32_${splits[3]}`;
+        await fs.rename(oldWinInstallerPath, newWinInstallerPath);
+        console.log(`Renamed ${oldWinInstallerPath} to ${newWinInstallerPath}`);
+    }
+})();
