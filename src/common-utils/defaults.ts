9f828dee9e3d4971371210a6d89ec50991887e9c
Rinse <rinse12@tutanota.com>
2024-12-09 07:47:38 +0000

    Revert "refactor(ipfs): change default ipfs port to 50019"
    This reverts commit 2e00f57e2c827ac35a23a34fd5fac069e182b153.

--- src/common-utils/defaults.ts
+++ src/common-utils/defaults.ts
@@ -4,7 +4,7 @@ import envPaths from "env-paths";
 export default {
     PLEBBIT_DATA_PATH: envPaths("plebbit", { suffix: "" }).data,
     PLEBBIT_RPC_URL: new URL("ws://localhost:9138"),
-    IPFS_API_URL: new URL("http://127.0.0.1:50019/api/v0"),
+    IPFS_API_URL: new URL("http://127.0.0.1:5001/api/v0"),
     IPFS_GATEWAY_URL: new URL("http://127.0.0.1:6473"),
     HTTP_TRACKERS: ["https://peers.pleb.bot", "https://routing.lol"],
     PLEBBIT_LOG_PATH: envPaths("plebbit", { suffix: "" }).log
