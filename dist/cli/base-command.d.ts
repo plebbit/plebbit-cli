addf970553ad58ad097e269a811b31926ff617cb
Rinse12 <Rinse12@users.noreply.github.com>
2025-12-26 11:46:18 +0000

    build(packaging): ci build

--- dist/cli/base-command.d.ts
+++ dist/cli/base-command.d.ts
@@ -1,7 +1,10 @@
 import { Command } from "@oclif/core";
+import Plebbit from "@plebbit/plebbit-js";
+type PlebbitInstance = Awaited<ReturnType<typeof Plebbit>>;
 export declare abstract class BaseCommand extends Command {
     static baseFlags: {
         plebbitRpcUrl: import("@oclif/core/interfaces").OptionFlag<import("url").URL, import("@oclif/core/interfaces").CustomOptions>;
     };
-    protected _connectToPlebbitRpc(plebbitRpcUrl: string): Promise<any>;
+    protected _connectToPlebbitRpc(plebbitRpcUrl: string): Promise<PlebbitInstance>;
 }
+export {};
--- dist/cli/base-command.js
+++ dist/cli/base-command.js
@@ -1,5 +1,6 @@
 import { Command, Flags } from "@oclif/core";
 import defaults from "../common-utils/defaults.js";
+import Plebbit from "@plebbit/plebbit-js";
 export class BaseCommand extends Command {
     static baseFlags = {
         plebbitRpcUrl: Flags.url({
@@ -9,8 +10,7 @@ export class BaseCommand extends Command {
         })
     };
     async _connectToPlebbitRpc(plebbitRpcUrl) {
-        const Plebbit = await import("@plebbit/plebbit-js");
-        const plebbit = await Plebbit.default({ plebbitRpcClientsOptions: [plebbitRpcUrl] });
+        const plebbit = await Plebbit({ plebbitRpcClientsOptions: [plebbitRpcUrl] });
         plebbit.on("error", (err) => console.error("Error from plebbit instance", err));
         await new Promise((resolve) => plebbit.once("subplebbitschange", resolve));
         return plebbit;
